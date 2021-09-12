cfa1b4f753430699a32b4c4f6452bc4703ba7abd
AShilina <twelidus@gmail.com>
2021-09-12 23:18:24 +0300

    Section 7 lesson (v2)

--- src/tests/MyListsTests.java
+++ src/tests/MyListsTests.java
@@ -32,6 +32,43 @@ public class MyListsTests extends CoreTestCase
             ArticlePageObject.addArticleToMyList(name_of_folder);
         } else {
             ArticlePageObject.addArticlesToMySaved();
+        }
+
+        if(Platform.getInstance().isIOS()) {
+            MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+            MyListsPageObject.closeSyncSavedArticlesPopUp();
+        }
+
+        ArticlePageObject.closeArticle();
+
+        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
+        NavigationUI.clickMyLists();
+
+        MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+
+        if (Platform.getInstance().isAndroid()) {
+            MyListsPageObject.openFolderByName(name_of_folder);
+        }
+
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+    }
+
+    @Test
+    public void testAddingT()
+    {
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        if(Platform.getInstance().isAndroid()) {
+            ArticlePageObject.addArticleToMyList(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
             MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
             MyListsPageObject.closeSyncSavedArticlesPopUp();
         }
